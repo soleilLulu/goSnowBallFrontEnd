b8508164715f40d8c2985408919c6c2d8a9522a1
soleil <705452492@qq.com>
2018-06-10 20:37:30 +0800

    feature(project)项目初始化

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- <login></login> -->
+    <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+// import Login from './pages/login'
+import NavBar from './components/navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    NavBar
+  }
+}
+</script>
+
+<style>
+body{
+  height: 100%;
+  width: 100%;
+  background-image: url('./assets/snow-ball-login-bg.jpg')
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 500px; */
+}
+</style>
---
+++ src/pages/login.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="login">
+    <div class="filedSet">
+        <legend class="legend">登录滚雪球</legend>
+        <div class="inputGroup">
+            <el-input class="input" placeholder="请输入用户名" v-model="input21"></el-input>
+            <el-input class="input" placeholder="请输入密码" v-model="input21" type="password"></el-input>
+            <el-button type="primary" plain>登录</el-button>
+        </div>
+    </div>
+</div>
+</template>
+<script>
+export default {
+    name: 'Login',
+    components: {
+  }
+}
+</script>
+<style lang="less" scoped>
+.login {
+    display: flex;
+    justify-content: center;
+    margin-top: 250px;
+    height: 100%;
+    align-items: center;
+}
+.filedSet {
+    height: 250px;
+    width: 400px;
+    background-color: #fff;
+    border-radius: 10px;
+    border: 1px solid #f0f0f0;
+    .legend{
+        box-sizing: border-box;
+        color: #fff;
+        width: 100%; 
+        background: #FF7052;
+        padding: 15px;
+        font-size: 20px;
+    }
+    .input {
+        padding: 10px;
+        margin: 0 auto;
+        width: 80%;
+    }
+}
+</style>
